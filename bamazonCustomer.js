c24036963a3bae83b1da8f52f19cde904b44f6a4
blaws006 <blaws006>
2017-10-30 22:31:59 -0400

    inquirer prompt in place.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,16 +15,28 @@ var connection = mysql.createConnection({
 
 //Functions/////////////////////////////////////////////////////////
 //Checks if the connetion between the Node app and SQL database was sucessful/////////////////////////////////////////////////////////
-connection.connect(function(err){
-if (err) throw err;
+connection.connect(function (err) {
+    if (err) throw err;
     console.log("Connected as ID: " + connection.threadId);
     totalInventory();
 });
 //Displays entire database///////////////////////////////////////////////////////////
 function totalInventory() {
-    connection.query("SELECT * FROM products", function(err, res){
+    connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
         console.log(res);
-        connection.end()
+        bamazonOrder();
     });
-}
+};
+//Asks the user two quetions:///////////////////////////////////////////////////////// 
+//1. What's the ID of the item that they'd like to buy /////////////////////////////////////////////////////////
+//2. How many units would they like to buy/////////////////////////////////////////////////////////
+function bamazonOrder() {
+    inquirer.prompt({
+        type: "list",
+        name: "order",
+        message: "What would you like to buy today?",
+        choices: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+    });
+
+};
