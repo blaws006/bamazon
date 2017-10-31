fff8ad0b18cd59ab8ef293965759920209486c43
blaws006 <blaws006>
2017-10-30 22:02:53 -0400

    Connection established and able to display the entire inventory

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,6 +3,28 @@ var inquirer = require("inquirer");
 var mysql = require("mysql");
 
 //Global Variables/////////////////////////////////////////////////////////
+//Connecting to the SQL database
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: "3306",
+    user: "root",
+    password: "R3dsk1ns1!",
+    database: "bamazon"
 
+});
 
 //Functions/////////////////////////////////////////////////////////
+//Checks if the connetion between the Node app and SQL database was sucessful/////////////////////////////////////////////////////////
+connection.connect(function(err){
+if (err) throw err;
+    console.log("Connected as ID: " + connection.threadId);
+    totalInventory();
+});
+//Displays entire database///////////////////////////////////////////////////////////
+function totalInventory() {
+    connection.query("SELECT * FROM products", function(err, res){
+        if (err) throw err;
+        console.log(res);
+        connection.end()
+    });
+}
