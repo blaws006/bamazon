45f7b8e8338964ab9ab761b6e028f9d941773091
blaws006 <blaws006>
2017-11-01 23:08:30 -0400

    Part one of this assignment complete. Might work on the bonus if there's time to...

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -37,9 +37,9 @@ function bamazonOrder() {
         type: "list",
         name: "order",
         message: "What would you like to buy today?",
-        choices: ["Towels", "Horizon Zero Dawn", "Moana Doll",  "NBA 2K18", "4eva is a Mighty Long Time", "Shampoo", "Lego Set", "Beach House 3", "Destiny 2", "2008"]
-    }).then(function(choice){
-        switch(choice.order){
+        choices: ["Towels", "Horizon Zero Dawn", "Moana Doll", "NBA 2K18", "4eva is a Mighty Long Time", "Shampoo", "Lego Set", "Beach House 3", "Destiny 2", "2008"]
+    }).then(function (choice) {
+        switch (choice.order) {
             case "Towels":
             case "Horizon Zero Dawn":
             case "Moana Doll":
@@ -50,15 +50,43 @@ function bamazonOrder() {
             case "Beach House 3":
             case "Destiny 2":
             case "2008":
-            orderList.push(choice.order);
-            howMany();
-            break;
-            default: 
-            console.log("Order number not recognized")
-        };        
+                orderList.push(choice.order);
+                howMany();
+                break;
+            default:
+                console.log("Order number not recognized")
+        };
     });
 
 };
+//Inquirer used to ask how many of the product that they will be ordering/////////////////////////////////////////////////////////
 function howMany() {
-    console.log("You bought " + orderList[0]);
+    inquirer.prompt({
+        type: "input",
+        name: "quantity",
+        message: "How many would you like to order?"
+    }).then(function (quant) {
+        connection.query("SELECT * FROM products WHERE product_name = ?", [orderList[0]], function (err, res) {//Rejects the transaction if quanity ordered is greater than quantity in stock
+            if (res[0].stock_quantity < quant.quantity) {
+                console.log("Insufficent amount!")
+                connection.end();
+            } else {//Logs the transaction and updates the db if acceptable
+                console.log("Transaction successful!")
+               var newQuant = res[0].stock_quantity -= quant.quantity;
+               var price = res[0].price *= quant.quantity;
+               console.log(newQuant)
+                var query = connection.query("UPDATE products SET stock_quantity = ? WHERE item_id = ?", [newQuant, res[0].item_id], function (err, res) {
+                    if (err) throw err;
+                    //Gives costomer the cost of their order
+                    console.log("Your total cost is $" + price);
+                    connection.end();
+                });
+
+            }
+
+        });
+    });
 }
+
+
+   
