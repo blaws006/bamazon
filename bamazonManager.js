112f8f9421dc3b8d3430ef1ac4b1b461e9ee22f1
blaws006 <blaws006>
2017-11-03 23:45:45 -0400

    Part two done. To be continued...

--- bamazonManager.js
+++ bamazonManager.js
@@ -42,8 +42,7 @@ function managerView() {
                 console.log("Add");
                 break;
             case "Add New Product":
-                // newProduct();
-                console.log("New Product");
+                newProduct();
         }
     })
 
@@ -76,7 +75,7 @@ function addInventory() {
         name: "refill",
         message: "What would you like to order today?",
         choices: ["Towels", "Horizon Zero Dawn", "Moana Doll", "NBA 2K18", "4eva is a Mighty Long Time", "Shampoo", "Lego Set", "Beach House 3", "Destiny 2", "2008"]
-         
+
     }).then(function (choice) {
         orderList.push(choice.refill);
         inquirer.prompt({
@@ -87,10 +86,46 @@ function addInventory() {
             connection.query("SELECT * from products WHERE product_name = ?", [orderList[0]], function (err, res) {
                 var newQuant = res[0].stock_quantity += parseInt(replenish.quantity);
                 connection.query("UPDATE products SET stock_quantity = ? WHERE item_id = ?", [newQuant, res[0].item_id], function (err, res) {
-                  console.log(newQuant);
-                  connection.end(); 
-                });  
+                    console.log(newQuant);
+                    connection.end();
+                });
             });
         });
     });
-}
+};
+
+function newProduct() {
+    inquirer.prompt([{
+            type: "input",
+            name: "product_name",
+            message: "What's the product?"
+        },
+        {
+            type: "input",
+            name: "department_name",
+            message: "What department does this belong to?"
+        },
+        {
+            type: "input",
+            name: "price",
+            message: "What is the cost of this product?"
+        },
+        {
+            type: "input",
+            name: "stock_quantity",
+            message: "How many do you want to order initially?"
+        }
+    ]).then(function (add) {
+        var post = {
+            product_name: add.product_name,
+            department_name: add.department_name,
+            price: add.price,
+            stock_quantity: add.stock_quantity
+        }
+        connection.query("INSERT INTO products SET ?", post, function (err, res) {
+            if (err) throw err;
+            console.log(post);
+            connection.end();
+        });
+    });
+};
